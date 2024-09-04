6c85ee89fa4c296f5041635349bb1db29ba31804
Nawanop-AMNB <ka.nawanop@gmail.com>
2024-09-05 02:27:18 +0700

    update viewport component

--- src/libs/view-port.tsx
+++ src/libs/view-port.tsx
@@ -1,8 +1,11 @@
 import { Box, styled } from "@mui/material";
 
 export const Viewport = styled(Box)`
+  background-color: white;
   ${({ theme }) => ({
-    backgroundColor: theme.palette.grey[200],
+    ...theme.applyStyles("light", {
+      backgroundColor: theme.palette.grey[200],
+    }),
     ...theme.applyStyles("dark", {
       backgroundColor: theme.palette.background.default,
     }),
